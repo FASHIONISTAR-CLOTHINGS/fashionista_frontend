e0cb4c9cf140837147b5dfb9acd273a325d17dc9
zichdan <zichdan1999@gmail.com>
2026-06-03 04:43:51 +0100

    chore(sync): synchronize industrial infrastructure optimizations and log maintenance (V82)

--- src/app/vendor/products/layout.tsx
+++ src/app/vendor/products/layout.tsx
@@ -1,11 +1,9 @@
 import type { ReactNode } from "react";
-import { VendorProductBuilderWrapper } from "./VendorProductBuilderWrapper";
 
 export default function VendorProductsLayout({
   children,
 }: {
   children: ReactNode;
 }) {
-  return <VendorProductBuilderWrapper>{children}</VendorProductBuilderWrapper>;
-  // return <>{children}</>;
+  return <>{children}</>;
 }
